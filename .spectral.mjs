735c8d4ad29ada82bd54d85b2cec22320c51e02c
qa-scfev-3-wkfd@example.com <qa-scfev-3-wkfd@example.com>
2023-03-22 17:23:13 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15767.stoplight-dev.com/api/v1/projects/cHJqOjE5OQ/spectral.js?branch_slug=main&token=f5c7c590-24f4-4407-b2bd-fddc7358f477";
+export default { extends: ruleset };
